59bb7c96b41704da66ddaca1c2c446b73962ed93
Peter Spegel <peter.spegel@acino.se>
2017-06-13 21:44:21 +0200

    Adds welcome page (wip)

---
+++ resources/lang/sv/main.php
@@ -0,0 +1,33 @@
+<?php
+
+return [
+
+    /*
+    |--------------------------------------------------------------------------
+    | Authentication Language Lines
+    |--------------------------------------------------------------------------
+    |
+    | The following language lines are used during authentication for various
+    | messages that we need to display to the user. You are free to modify
+    | these language lines according to your application's requirements.
+    |
+    */
+
+    'start' => 'Start',
+    'jujutsu' => 'Jujutsu',
+    'kickboxing' => 'Kickboxning',
+    'bjj' => 'Gracie Barra BJJ',
+    'schedule' => 'Schema',
+
+    'welcomeHeading' => 'Välkommen till Umeå Kampcenter',
+    'welcomeText' => 'Umeå Kampcenter erbjuder tre kompletta kampsportsstilar: Jujutsu, Kickboxning och '
+        . 'Gracie Barra BJJ. Alla dessa kampsporter kompletterar varandra och som medlem får du får träna alla tre! '
+        . 'Medlemsavgiften är 800kr/termin. Självklart får du pröva på några tillfällen innan du betalar!',
+
+    'jujutsuIntro' => 'Jujutsu är självförsvar med kampmoment som brottning, kast, stående fighting och självskydd. '
+        . 'Just nu har vi endast vuxenverksamhet (14+).',
+    'kickboxingIntro' => 'Kickboxning går ut på stående kamp med slag & spark.',
+    'bjjIntro' => 'Gracie Barra BJJ fokuserar på liggande markteknik och mattkamp.',
+    'readMore' => 'Läs mer'
+
+];
---
+++ serve.bat
@@ -0,0 +1,1 @@
+php artisan serve --host=192.168.1.148 --port=80
