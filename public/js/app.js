a9f8b69a202b236deb6cc325483a7c6b4f8d6c2b
Peter Spegel <peter.spegel.umea@gmail.com>
2017-07-07 00:35:51 +0200

    Rensar bort lite exempelfiler. Förbereder för stagening.

--- public/js/app.js
+++ public/js/app.js
@@ -2,8 +2,6 @@ let umeakampcenter = window.umeakampcenter || {};
 umeakampcenter.menu = (function() {
     let pub = {};
     pub.init = function ($button, $container) {
-        console.log($button);
-        console.log($container);
         $button.click(function () {
             $container.slideToggle();
         });
