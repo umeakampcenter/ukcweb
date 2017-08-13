e9127f7bd41c1aefc7325a56b29d02c53837a97a
Peter Spegel <peter.spegel@acino.se>
2017-08-13 21:29:15 +0200

    Tar bort lite förkortningar.

--- resources/lang/en/main.php
+++ resources/lang/en/main.php
@@ -21,14 +21,14 @@ return [
 
     'welcomeHeading' => 'Welcome to Umeå Kampcenter',
     'welcomeText' => 'Umeå Kampcenter offers three complete martial arts styles: Jujutsu, Kickboxing and '
-        . 'Gracie Barra BJJ. Our styles complement each other and as a member you get to train all three! '
+        . 'Gracie Barra Brazilian Jiu-Jitsu. Our styles complement each other and as a member you get to train all three! '
         . 'The membership fee is 1000 SEK per semester. Naturally you can try them out a few times before you become '
         . 'a member! At this time we accept students that are 14 years or older.',
 
     'jujutsuIntro' => 'Jujutsu is self defence with elements such as ground work, throws, standup fighting and joint '
         . 'locks.',
     'kickboxingIntro' => 'Kickboxing is standup fighting with punches and kicks.',
-    'bjjIntro' => 'Gracie Barra BJJ focuses on ground work and fighting on the ground.',
+    'bjjIntro' => 'Gracie Barra Brazilian Jiu-Jitsu focuses on ground work and fighting on the ground.',
     'readMore' => 'Read more',
     'seeSelfDefence' => 'Self defence for girls',
     'seeTrainingSessions' => 'Look at our training sessions',
--- resources/lang/sv/main.php
+++ resources/lang/sv/main.php
@@ -21,13 +21,14 @@ return [
 
     'welcomeHeading' => 'Välkommen till Umeå Kampcenter',
     'welcomeText' => 'Umeå Kampcenter erbjuder tre kompletta kampsportsstilar: Jujutsu, Kickboxning och '
-        . 'Gracie Barra BJJ. Alla dessa kampsporter kompletterar varandra och som medlem får du får träna alla tre! '
+        . 'Gracie Barra Brazilian Jiu-Jitsu. Alla dessa kampsporter kompletterar varandra och som medlem får du får '
+        . 'träna alla tre! '
         . 'Medlemsavgiften är 1000kr/termin. Självklart får du pröva på några tillfällen innan du betalar! '
         . 'Just nu har vi endast vuxenverksamhet (14+).',
 
     'jujutsuIntro' => 'Jujutsu är självförsvar med kampmoment som brottning, kast, stående fighting och självskydd.',
     'kickboxingIntro' => 'Kickboxning går ut på stående kamp med slag & spark.',
-    'bjjIntro' => 'Gracie Barra BJJ fokuserar på liggande markteknik och mattkamp.',
+    'bjjIntro' => 'Gracie Barra Brazilian Jiu-Jitsu fokuserar på liggande markteknik och mattkamp.',
     'readMore' => 'Läs mer',
     'seeSelfDefence' => 'Självförsvar för tjejer',
     'seeTrainingSessions' => 'Se våra träningstider',
