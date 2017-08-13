ed5455ca1fdba8d3bba172ce5605d5abfb3a5f2f
Peter Spegel <peter.spegel@acino.se>
2017-08-13 21:52:43 +0200

    Lägger till terminsstarter.

--- resources/views/front.blade.php
+++ resources/views/front.blade.php
@@ -17,16 +17,19 @@
         <div class="section-item">
             <h2>@lang('main.jujutsu')</h2>
             <div class="section-text">@lang('main.jujutsuIntro')</div>
+            <div class="section-text-start">@lang('main.jujutsuStart')</div>
             <a href="{{ route('jujutsu') }}">@lang('main.readMore')</a>
         </div>
         <div class="section-item">
             <h2>@lang('main.kickboxing')</h2>
             <div class="section-text">@lang('main.kickboxingIntro')</div>
+            <div class="section-text-start">@lang('main.kickboxingStart')</div>
             <a href="{{ route('kickboxing') }}">@lang('main.readMore')</a>
         </div>
         <div class="section-item">
             <h2>@lang('main.bjj')</h2>
             <div class="section-text">@lang('main.bjjIntro')</div>
+            <div class="section-text-start">@lang('main.bjjStart')</div>
             <a href="{{ route('bjj') }}">@lang('main.readMore')</a>
         </div>
     </div>
