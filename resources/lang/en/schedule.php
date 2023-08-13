11fd7a6059e825aa96c03344424d2019a7eacf58
Peter Spegel <peter.spegel.umea@gmail.com>
2023-08-13 22:17:09 +0200

    Tar bort oanvänt språk.

--- resources/lang/en/schedule.php
+++ resources/lang/en/schedule.php
@@ -7,30 +7,7 @@ return [
     "thursday" => "thursday",
     "friday" => "friday",
     "saturday" => "saturday",
-    "sunday" => "sunday",
-
-    "jujutsuBegin" => "Jujutsu beginners",
-    "jujutsuAdvance" => "Jujutsu advance",
-    "jujutsu" => "Jujutsu beginners & advance",
-    "jujutsuSparring" => "Jujutsu standup sparring",
-    "booked" => "Booked",
-    "bjjOpen" => "BJJ free training",
-    "bjjBeginAdvance" => "BJJ beginners & advance",
-    "bjjAdvance" => "BJJ advance",
-    "bjjIntermediate" => "BJJ intermediate",
-    "bjjBegin" => "BJJ beginners",
-    "bjjIntermediateAdvance" => "BJJ intermediate & advance",
-    "bjjSpecific" => "BJJ drilling",
-    "bjjComp" => "BJJ competition training",
-    "bjjNogi" => "BJJ no-gi",
-    "bjjDrill" => "BJJ drill",
-    "bjjKids" => "BJJ children 6-9 years (play, basics)",
-    "bjjYouth" => "BJJ youths 9-13 years (technique, sparring)",
-    "bjjSparring" => "BJJ sparring",
-    "bjjTech" => "BJJ technique",
-    "sw" => "Submission wrestling",
-    "selfDefence" => "Self defence for women and transgenders",
-    "open" => "Open mat"
+    "sunday" => "sunday"
 ];
 
 
--- resources/lang/sv/schedule.php
+++ resources/lang/sv/schedule.php
@@ -7,30 +7,7 @@ return [
     "thursday" => "torsdag",
     "friday" => "fredag",
     "saturday" => "lördag",
-    "sunday" => "söndag",
-
-    "jujutsuBegin" => "Jujutsu nybörjare",
-    "jujutsuAdvance" => "Jujutsu avance",
-    "jujutsu" => "Jujutsu nybörjare & avance",
-    "jujutsuSparring" => "Jujutsu stående sparring",
-    "booked" => "Bokat",
-    "bjjOpen" => "BJJ friträning",
-    "bjjBeginAdvance" => "BJJ nybörjare & avance",
-    "bjjAdvance" => "BJJ avance",
-    "bjjIntermediate" => "BJJ fortsättare",
-    "bjjBegin" => "BJJ nybörjare",
-    "bjjIntermediateAdvance" => "BJJ fortsättare & avance",
-    "bjjSpecific" => "BJJ drillpass",
-    "bjjComp" => "BJJ tävlings&shy;träning",
-    "bjjNogi" => "BJJ no-gi",
-    "bjjDrill" => "BJJ drill",
-    "bjjKids" => "BJJ barn 6-9 år (lek, grunder)",
-    "bjjYouth" => "BJJ ungdomar 9-13 år (teknik, sparring)",
-    "bjjSparring" => "BJJ sparring",
-    "bjjTech" => "BJJ teknik",
-    "sw" => "Submission wrestling",
-    "selfDefence" => "Självförsvar för tjejer och transpersoner",
-    "open" => "Öppen matta"
+    "sunday" => "söndag"
 ];
 
 
