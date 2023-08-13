4ff153b9e40ea79daf3d2c7d209656e2ecfe606a
Peter Spegel <peter.spegel.umea@gmail.com>
2023-08-13 22:28:34 +0200

    Tar bort oanvänt språk.

--- resources/lang/en/main.php
+++ resources/lang/en/main.php
@@ -1,38 +1,19 @@
 <?php
 
 return [
-
-    /*
-    |--------------------------------------------------------------------------
-    | Authentication Language Lines
-    |--------------------------------------------------------------------------
-    |
-    | The following language lines are used during authentication for various
-    | messages that we need to display to the user. You are free to modify
-    | these language lines according to your application's requirements.
-    |
-    */
-
     'start' => 'Home',
     'train' => 'Train with us',
     'jujutsu' => 'Jujutsu',
     'bjj' => 'Gracie Barra BJJ',
     'selfDefence' => 'Self-defence for women',
-    'selfDefenceWithLineBreak' => 'Self-defence<br>for women',
     'bjjChildren' => 'BJJ barnträning',
     'board' => 'Board',
     'schedule' => 'Schedule',
     'pricing' => 'Pricing',
-
-    'semesters' => 'Spring semester is week 3-23 and autumn week 35-50.',
     'readMore' => 'Read more',
-    'seeTrainingSessions' => 'Look at our training sessions',
-    'showPhotos' => 'Show photos',
-
     'allRightsReserved' => 'All rights reserved',
     'findUs' => 'Find us',
     'email' => 'Send an email to us',
-
     'membershipFee' => 'The membership fee is 1100 SEK per semester. Members have access to all our styles!',
     'trainingTimes' => 'Look at our training sessions'
 ];
--- resources/lang/sv/main.php
+++ resources/lang/sv/main.php
@@ -1,38 +1,19 @@
 <?php
 
 return [
-
-    /*
-    |--------------------------------------------------------------------------
-    | Authentication Language Lines
-    |--------------------------------------------------------------------------
-    |
-    | The following language lines are used during authentication for various
-    | messages that we need to display to the user. You are free to modify
-    | these language lines according to your application's requirements.
-    |
-    */
-
     'start' => 'Start',
     'train' => 'Träna hos oss',
     'jujutsu' => 'Jujutsu',
     'bjj' => 'Gracie Barra BJJ',
     'selfDefence' => 'Självförsvar för tjejer',
-    'selfDefenceWithLineBreak' => 'Självförsvar<br>för tjejer',
     'bjjChildren' => 'BJJ barnträning',
     'board' => 'Styrelse',
     'schedule' => 'Schema',
     'pricing' => 'Priser',
-
-    'semesters' => 'Vårterminen är vecka 3-23 och höstterminen är vecka 35-50.',
     'readMore' => 'Läs mer',
-    'seeTrainingSessions' => 'Se våra träningstider',
-    'showPhotos' => 'Visa bilder',
-
     'allRightsReserved' => 'Alla rättigheter reserverade',
     'findUs' => 'Hitta oss',
     'email' => 'Skicka e-post till oss',
-
     'membershipFee' => 'Medlemsavgiften är 1100kr/termin. Som medlem får du träna alla våra stilar!',
     'trainingTimes' => 'Se våra träningstider'
 ];
