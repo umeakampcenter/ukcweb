0b87f3c7d83514e5ca5a8014d7f2e7217cccd313
Peter Spegel <peter.spegel.umea@gmail.com>
2025-05-25 19:54:50 +0200

    Tar bort gamla filer.

--- resources/lang/en/sub.php
+++
@@ -1,7 +0,0 @@
-<?php
-
-return [
-    "instructors" => "Instructors",
-    "tryOutText" => "You're always welcome to try out our different styles without advance notification. You can "
-        . "train two full weeks before the membership fee is due. As a member you can train all our styles if you wish."
-];
--- resources/lang/sv/sub.php
+++
@@ -1,8 +0,0 @@
-<?php
-
-return [
-    "instructors" => "Instruktörer",
-    "tryOutText" => "Du är alltid välkommen att prova på, ingen föranmälan krävs. På vår klubb får du träna i två "
-        . "veckor innan du behöver betala medlemsavgiften. Som medlem hos oss får du träna alla våra stilar om du "
-        . "önskar."
-];
